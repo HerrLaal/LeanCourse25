8951ebe5e936d7756c04eb4536ca438566e5f4f3
herpa <laalherr@gmail.com>
2025-12-13 13:49:55 +0100

    small clean up of Prereq

--- LeanCourse25/Projects/Prereq/Mathlib/Combinatorics/SimpleGraph/Clique.lean
+++ LeanCourse25/Projects/Prereq/Mathlib/Combinatorics/SimpleGraph/Clique.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Bhavik Mehta
 -/
 
-import ExponentialRamsey.Prereq.Mathlib.Combinatorics.SimpleGraph.Basic
+import LeanCourse25.Projects.Prereq.Mathlib.Combinatorics.SimpleGraph.Basic
 import Mathlib.Combinatorics.SimpleGraph.Basic
 import Mathlib.Combinatorics.SimpleGraph.Clique
 
