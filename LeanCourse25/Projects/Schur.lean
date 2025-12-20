437fa2912e8bfd79b7d9863cf54e0c9264258e9b
HerrLaal <laalherr@gmail.com>
2025-12-20 16:55:38 +0100

    adds skeleton of the end of the proof of Schur's theorem

--- LeanCourse25/Projects/Schur.lean
+++ LeanCourse25/Projects/Schur.lean
@@ -33,9 +33,24 @@ theorem schur (c : ℕ) (hc : c > 0) :
     fun x ↦ i (max (x.1.out.fst - x.1.out.snd) (x.1.out.snd - x.1.out.fst))
     --TODO: maybe we can find an appropriate norm function
   specialize ramval tel
-  obtain ⟨p, c1, elm, hp2⟩ := ramval
-  have: #p ≥ 3 := by (have: n c1 ≥ 3 := by gcongr); gcongr
+  obtain ⟨p, c1, elm, hp⟩ := ramval
   use p.toSet
   constructor
   · sorry
-  · sorry
+  · have : #p ≥ 3 := by gcongr
+    have : ∃ x ∈ p, ∃ y ∈ p, ∃ z ∈ p, x ≠ y ∧ y ≠ z ∧ x ≠ z := by sorry
+    obtain ⟨x, hx, y, hy, z, hz, hxy, hyz, hxz⟩ := this
+    use x --TODO: use a better structure than combining constructors
+    constructor
+    · exact hx
+    · use y
+      constructor
+      · exact hy
+      · have: z = x + y := by
+         simp at elm
+         have elmxy : EdgeLabelling.get tel x y hxy = c1 := by specialize elm hx hy hxy; exact elm
+         have elmyz : EdgeLabelling.get tel y z hyz = c1 := by specialize elm hy hz hyz; exact elm
+         have elmxz : EdgeLabelling.get tel x z hxz = c1 := by specialize elm hx hz hxz; exact elm
+         simp[tel] at elmxy elmyz elmxz
+         sorry
+        exact Set.mem_of_eq_of_mem (id (Eq.symm this)) hz
