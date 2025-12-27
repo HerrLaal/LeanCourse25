d7b318cb0bd526332ec1f6c055ef951b2de20aa5
HerrLaal <laalherr@gmail.com>
2025-12-27 18:03:45 +0100

    clean up

--- LeanCourse25/Projects/Schur.lean
+++ LeanCourse25/Projects/Schur.lean
@@ -17,9 +17,7 @@ theorem schur (c : ℕ) (hc : c > 0) :
   ∃ S, ∀ (C : Finset (Set (Fin S))),
   (IsPartition C.toSet ∧ #C = c) → ∃ a ∈ C, ∃ x ∈ a, ∃ y ∈ a, ∃ z ∈ a, x + y = z := by
   let n : (Fin c → ℕ) := fun _ ↦ 3
-  have ramval : (∃ N, ∀ D : TopEdgeLabelling (Fin N) (Fin c), ∃ (p : Finset (Fin N)) (color : _),
-   D.MonochromaticOf p color ∧ n color ≤ p.card) := by apply ramsey_fin_exists n
-  obtain ⟨N, ramval⟩ := ramval
+  let N:= ramseyNumber n
   use N
   simp
   intro C PartC CardC
@@ -33,38 +31,21 @@ theorem schur (c : ℕ) (hc : c > 0) :
   let tel : (TopEdgeLabelling (Fin N) (Fin c)) :=
     fun x ↦ Ctoc.1 (index (max x.1.out.fst x.1.out.snd - min x.1.out.fst x.1.out.snd))
     --TODO: maybe we can find an appropriate norm function
+  have ramval : ∀ D : TopEdgeLabelling (Fin N) (Fin c), ∃ (p : Finset (Fin N)) (color : _),
+    D.MonochromaticOf p color ∧ n color ≤ p.card := by apply ramseyNumber_spec_fin n
   specialize ramval tel
   obtain ⟨p, c1, elm, hp⟩ := ramval
-  use p.toSet
-  constructor
-  · sorry
-  · have Cardp: #p ≥ 3 := by gcongr
-    have : ∃ i ∈ p, ∃ j ∈ p, ∃ k ∈ p, i > j ∧ j > k ∧ i > k := by
+  have Cardp: #p ≥ 3 := by gcongr
+  have : ∃ i ∈ p, ∃ j ∈ p, ∃ k ∈ p, i > j ∧ j > k ∧ i > k := by
       sorry
-    obtain ⟨i, hi, j, hj, k, hk, hij, hjk, hik⟩ := this
-    use i - j --TODO: use a better structure than combining constructors
+  obtain ⟨i, hi, j, hj, k, hk, hij, hjk, hik⟩ := this
+  use index (i - j)
+  constructor
+  · exact coe_mem (index (i - j))
+  · use i - j
     constructor
-    · simp  -- Type 1
-      have elmij : tel ⟨s(i, j), (Ne.symm (Fin.ne_of_lt hij))⟩ = c1 := by
-          specialize elm hi hj (Ne.symm (Fin.ne_of_lt hij)); exact elm
-      have quoti: (Quot.out s(i, j)).1 = i := by sorry  -- Type 2
-      have quotj: (Quot.out s(i, j)).2 = j := by sorry  -- Type 2
-      have : Ctoc.1 (index (i - j)) = c1 := by
-        rw[←elmij]
-        unfold tel
-        simp
-        rw??
-        rw[quoti, quotj]
-        rw [max_eq_left ?_]
-        rw [min_eq_right ?_]
-        gcongr
-        exact Fin.le_of_lt hij
-
-      sorry  -- Type 1
+    · sorry
     · use j - k
       constructor
-      · sorry -- Type 1
-      · simp
-        have:  i - k = i - j + (j - k):= by sorry
-        rw[←this]
-        sorry  -- Type 1
+      · sorry
+      · sorry
